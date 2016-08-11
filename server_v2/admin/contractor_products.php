afc3fed845ab0e373e5c1094fb160b19d0fae445
Сотников Степан Егорович <skewlife@gmail.com>
2016-08-11 17:36:39 +0900

    11082016 empty commit for merge

--- server_v2/admin/contractor_products.php
+++ server_v2/admin/contractor_products.php
@@ -18,25 +18,106 @@
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
-    <!-- Latest compiled and minified JQuery -->
-    <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="../libs/json-editor/jsoneditor.js"></script>
 </head>  
 <body>
+<?php
+require_once dirname(__FILE__).'/../include/DbHandlerFabricant.php';
+session_start();
+include 'auth.php';
 
-<?php 
-//echo "Server for Connection, Session, Uploader, Communicator, Profile, Chat, Messenger project's demo. Look here https://github.com/Igorpi25/";
+if (!isset($_GET['id'])){
+	header("Location: http://".$_SERVER['HTTP_HOST']."/fabricant/server_v2/admin/contractors.php"); //"/v2/admin/contractors.php");
+	exit;
+}
+$fdb = new DbHandlerFabricant();
+$contractorProducts=$fdb->getProductsOfContractor($_GET['id']);
+if($contractorProducts==NULL) {
+	header("Location: http://".$_SERVER['HTTP_HOST']."/fabricant/server_v2/admin/contractors.php"); //"/v2/admin/contractors.php");
+	exit;
+}
 
-if (isset($_GET['id'])){
-	session_start();
-	include 'auth.php';
-	require_once dirname(__FILE__).'/../include/DbHandlerFabricant.php';
-	$fdb = new DbHandlerFabricant();
-	$contractorProducts=$fdb->getProductsOfContractor($_GET['id']);
-	print_r($contractorProducts);
-}
-else {
-	echo "not post";
-}
+//print_r($contractorProducts);
 ?>
+<div class="container">
+	<h2>Products</h2>
+    <p>Contractor products</p>
+    <table class="table">
+	<thead>
+		<tr>
+			<th>#</th>
+			<th>contractorid</th>
+			<th>name</th>
+			<th>status</th>
+			<th>price</th>
+            <!--th>info</th-->
+            <th>changed at</th>
+		</tr>
+	</thead>
+	<tbody>
+	<?php
+	$infos=array();
+	foreach($contractorProducts as $product) {
+		echo '<tr>
+			<td>'.$id=$product["id"].
+			'</td><td>'.$contractorid=$product["contractorid"].
+			'</td><td>'.$name=$product["name"].
+			'</td><td>'.$status=$product["status"].
+			'</td><td>'.$price=$product["price"].
+			//'</td><td>'.$info=$product["info"].
+			'</td><td>'.$changed_at=$product["changed_at"].
+			'</td><tr>';
+			$infos[]=$product["info"];
+	}
+	$test=array('a'=>1);
+	echo '<script> var infos='.json_encode($test).'</script>';
+	?>
+	</tbody>
+	</table>
+    <div id="testdiv" class="row" style="background:#EBEBEB">
+    	
+    </div>
+</div>
+<script>
+	// Initialize the editor with a JSON schema
+      /*var editor = new JSONEditor(document.getElementById('editor_holder'),{
+        schema: {
+          type: "object",
+          title: "Car",
+          properties: {
+            make: {
+              type: "string",
+              enum: [
+                "Toyota",
+                "BMW",
+                "Honda",
+                "Ford",
+                "Chevy",
+                "VW"
+              ]
+            },
+            model: {
+              type: "string"
+            },
+            year: {
+              type: "integer",
+              enum: [
+                1995,1996,1997,1998,1999,
+                2000,2001,2002,2003,2004,
+                2005,2006,2007,2008,2009,
+                2010,2011,2012,2013,2014
+              ],
+              default: 2008
+            }
+          }
+        }
+      });*/
+	  $("#testdiv").append("asasdasdasddasssssssssssssssssssssas");
+	  infos=JSON.parse(infos);
+	  alert("mthfk");
+	  
+</script>
 </body>
 </html>
--- server_v2/admin/contractors.php
+++ server_v2/admin/contractors.php
@@ -1,86 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
-
-    <!-- Latest compiled and minified CSS -->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<title>Bootstrap 101 Template</title>
+	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
 	<!-- Optional theme -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>  
-  <body>
-  
-	<?php
-	session_start();
-	include 'auth.php';
-	//Provides db (the instance of DbProfileHandler.php)
-	?>
-	
-	<div class="page-header">
-		<h1>Админка</h1>
-	</div>
-
-	<h2><a href="http://igorserver.ru/v2/admin/contractors.php">Поставшики/</a></h2>
-	
-      <div class="row">
-        <div class="col-md-6">
-          <table class="table table-striped">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>id</th>
-				<th>name</th>
-                <th>details</th>
-                <th>products</th>
-              </tr>
-            </thead>
-            <tbody>
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>  
+<body>  
 <?php
-$groups=$db->getAllGroups();
-$i=1;
-foreach($groups as $group){
-	//Contractors
-	//if ($group["status"]==1) {
-		
-		echo '<tr>
-			<td>'.$i++.'</td>
-			<td>'.$group["id"].'</td>
-			<td>'.$group["name"].'</td>                
-			<td><a href="contractor_details?id='.$group["id"].'">details</a></td>
-			<td><a href="contractor_products.php?id='.$group["id"].'">products</a></td>
-		</tr>';
-		
-	//}		
-}
-
+session_start();
+include 'auth.php';
+//Provides db (the instance of DbProfileHandler.php)
 ?>
-              
-            </tbody>
-          </table>
-        </div>
-      </div>
-	  	  
-    <a href="add_group.php"><button id='add' type="button" class="btn btn-lg btn-default">Создать группу</button></a>
-	  
+<div class="page-header">
+	<h1>Админка</h1>
+</div>
+<h2><a href="http://igorserver.ru/v2/admin/contractors.php">Поставшики/</a></h2>
+<div class="row">
+	<div class="col-md-6">
+		<table class="table table-striped">
+		<thead>
+			<tr>
+				<th>#</th>
+				<th>id</th>
+				<th>name</th>
+				<th>details</th>
+				<th>products</th>
+			</tr>
+		</thead>
+		<tbody>
+        	<!--tr>
+				<th>0</th>
+				<th>11</th>
+				<th>asd</th>
+				<th ><a href="contractor_details?id=11">details</a></th>
+				<th><a href="contractor_products.php?id=11">products</a></th>
+			</tr-->
+			<?php
+			$groups=$db->getAllGroups();
+			$i=1;
+			foreach($groups as $group) {
+			//Contractors
+			//if ($group["status"]==1) {
+				echo '<tr>
+					<td>'.$i++.'</td>
+					<td>'.$group["id"].'</td>
+					<td>'.$group["name"].'</td>                
+					<td><a href="contractor_details?id='.$group["id"].'">details</a></td>
+					<td><a href="contractor_products.php?id='.$group["id"].'">products</a></td>
+				</tr>';
+			//}
+			}?>
+		</tbody>
+		</table>
+	</div>
+</div>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-  </body>
+	<a href="add_group.php"><button id='add' type="button" class="btn btn-lg btn-default">Создать группу</button></a>
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed>
+	<script src="js/bootstrap.min.js"></script-->
+</body>
 </html>
